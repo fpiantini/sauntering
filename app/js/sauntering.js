ccf15e0f4363fb7f2b8446c37f9b1eaac02eb467
Francesco Piantini <francesco.piantini@gmail.com>
2020-06-14 16:45:36 +0200

    added leaflet dependency; first 'raw' whole screen map version

---
+++ app/js/sauntering.js
@@ -0,0 +1,8 @@
+const { map, tileLayer } = require("leaflet");
+
+const mymap = map("mapid").setView([43.875248, 11.236878], 13);
+tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+  maxZoom: 17,
+  attribution:
+    'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+}).addTo(mymap);
